dab628dc06d3e4fbebb91dae2a36b89853d33eb2
Peter Simonsson <peter@simonsson.com>
2021-12-02 19:00:55 +0100

    Add day 1 challenge 2

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,4 +1,4 @@
-pub fn run(values: Vec<i32>) {
+pub fn part1(values: &Vec<i32>) -> i32 {
 	let mut increased = 0;
 
 	for (i, current) in values.iter().enumerate() {
@@ -6,15 +6,38 @@ pub fn run(values: Vec<i32>) {
 			continue;
 		}
 
-		match values.get(i - 1) {
-			Some(prev) => {
-				if current > prev {
-					increased += 1;
-				}
-			},
-			None => {}
+		if let Some(prev) = values.get(i - 1) {
+			if current > prev {
+				increased += 1;
+			}
 		}
 	}
 
-	println!("Day 1, answer: {}", increased);
+	return increased;
+}
+
+pub fn part2(values: &Vec<i32>) -> i32 {
+	let mut increased = 0;
+	let mut i = 0;
+	loop {
+		if let (Some(previous), Some(current)) = (sum_window(values, i, 3), sum_window(values, i + 1, 3)) {
+			if previous < current {
+				increased += 1;
+			}
+		} else {
+			break;
+		}
+
+		i += 1;
+	}
+
+	return increased;
+}
+
+fn sum_window(values: &Vec<i32>, index: usize, size: usize) -> Option<i32> {
+	if let Some(slice) = values.get(index..index+size) {
+		return Some(slice.iter().sum());
+	} else {
+		return None;
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@ mod common;
 mod day1;
 
 fn main() {
-    let values = common::read("src/day1/input.txt");
+    let day1input = common::read("src/day1/input.txt");
 
-    day1::run(values);
+    let day1part1 = day1::part1(&day1input);
+    let day1part2 = day1::part2(&day1input);
+    println!("DAY 1\nPart 1: {}\nPart 2: {}", day1part1, day1part2);
+
 }
