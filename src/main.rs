0fe2f84813415da59297ed314a865f7bdd2e9289
Peter Simonsson <peter@simonsson.com>
2021-12-02 19:07:29 +0100

    Assert solutions for day 1

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ mod common;
 mod day1;
 
 fn main() {
+    // DAY 1
     let day1input = common::read("src/day1/input.txt");
-
     let day1part1 = day1::part1(&day1input);
     let day1part2 = day1::part2(&day1input);
-    println!("DAY 1\nPart 1: {}\nPart 2: {}", day1part1, day1part2);
+    assert_eq!(day1part1, 1292);
+    assert_eq!(day1part2, 1262);
+    print_day("1", day1part1, day1part2);
+}
 
+fn print_day(day: &str, part1: i32, part2: i32) {
+    println!("DAY {}\nPart 1: {}\nPart 2: {}", day, part1, part2);
 }
